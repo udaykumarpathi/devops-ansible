f605200dac504174f6f90ed9c3787b3943c44f48
udaykumarpathi <43530505+udaykumarpathi@users.noreply.github.com>
2022-08-04 12:45:12 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -4,12 +4,12 @@ region = "us-east-2"
 resource "aws_instance" "myawsserver" {
   ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.micro"
-  key_name = "gagan-import"
+  key_name = "Uday-Key"
 
   tags = {
-    Name = "Gagan-DevOps-server"
-    env = "Prod"
-    owner = "Gagandeep Singh"
+    Name = "Uday-DevOps-server"
+    env = "Development"
+    owner = "Uday Pathi"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /tmp/inv"
